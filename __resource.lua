611c638049cf87cdb218efc92033507a13cabd86
viccy-FiveM <viccynulnegen@gmail.com>
2021-07-13 16:33:36 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,6 +2,11 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'FSN Gym'
 
+-- GLOBAL UTILS
+client_script '@fsn_main/cl_utils.lua'
+server_script '@fsn_main/sv_utils.lua'
+
+
 version '1.0.0'
 
 server_scripts {
